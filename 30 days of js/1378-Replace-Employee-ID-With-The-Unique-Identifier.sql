cd547a7fedeebe18e3fd8ff2189e64b131dcfc54
Ahmed Taha <95619180+ahmedtaha5@users.noreply.github.com>
2024-09-11 02:31:47 +0300

    [1378] [Time Beats: 96.30%] - LeetPush

--- 30 days of js/1378-Replace-Employee-ID-With-The-Unique-Identifier.sql
+++ 30 days of js/1378-Replace-Employee-ID-With-The-Unique-Identifier.sql
@@ -1,10 +1,3 @@
--- SELECT IFNULL(eu.unique_id, 'null') as unique_id, e.name
--- FROM Employees e 
--- left join EmployeeUNI eu ON e.id = eu.id;
-
-
-
-
 # Write your MySQL query statement below
 select un.unique_id ,e.name
 from Employees e
