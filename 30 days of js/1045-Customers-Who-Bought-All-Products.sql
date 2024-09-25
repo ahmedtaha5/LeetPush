c0b9eacd0a4f450ba40e80bee2535a94fafe6841
Ahmed Taha <95619180+ahmedtaha5@users.noreply.github.com>
2024-09-25 16:32:17 +0300

    [1045] [Time Beats: 98.02%] - LeetPush

--- 30 days of js/1045-Customers-Who-Bought-All-Products.sql
+++ 30 days of js/1045-Customers-Who-Bought-All-Products.sql
@@ -1,12 +1,4 @@
--- # Write your MySQL query statement below
--- SELECT customer_id 
--- FROM Customer c
--- JOIN Product p
--- on c.product_key = p.product_key
-
--- GROUP BY customer_id
-
 SELECT customer_id
-FROM Customer 
+FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
